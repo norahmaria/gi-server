c9e3846850cbc40e9aa3c5d70866534153013281
NorahSketch <norahmariano@gmail.com>
2021-12-02 17:47:47 +0100

    Cors Issues

--- src/routes/sign/in.ts
+++ src/routes/sign/in.ts
@@ -24,7 +24,7 @@ const route: Route = {
       const token = jwt.sign({ userId: user._id }, env.SECRET, { expiresIn: "1hr" })
       return res.status(200).cookie('token', token, {
         expires: new Date(Date.now() + 604800000),
-        secure: false,
+        secure: true,
         httpOnly: true
       }).send(user)
     } catch (error) {
--- src/routes/sign/up.ts
+++ src/routes/sign/up.ts
@@ -31,7 +31,7 @@ const route: Route = {
 
       return res.status(200).cookie('token', token, {
         expires: new Date(Date.now() + 604800000),
-        secure: false,
+        secure: true,
         httpOnly: true
       }).send(user)
     } catch (error) {
