429a4f03709bd317eef39d4e53fb2ec7123ca928
NorahSketch <norahmariano@gmail.com>
2021-12-06 16:29:44 +0100

    Session

--- src/config/createApp.ts
+++ src/config/createApp.ts
@@ -2,14 +2,29 @@ import type { Cors } from '../env/cors'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import corsPackage from 'cors'
+import session from 'express-session'
+import env from '../env/env'
 
 const createApp = (cors: Cors) => {
   const app = express()
 
   app.set("trust proxy", 1)
-  app.use(corsPackage(cors))
   app.use(express.json({ limit: '30mb' }))
+
+  app.use(corsPackage(cors))
   app.use(cookieParser())
+  
+  app.use(session({
+    secret: env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { 
+      secure: env.ENVIRONMENT === 'LIVE',
+      sameSite: env.ENVIRONMENT === 'LIVE' ? 'strict' : 'lax',
+      httpOnly: true, 
+      maxAge: 1000 * 60 * 60 * 24 
+    }
+  }))
 
   return app
 }
--- src/routes/sign/out.ts
+++ src/routes/sign/out.ts
@@ -9,6 +9,7 @@ const route: Route = {
   authorization,
   execute: async (req, res) => {
     try {
+      req.session.destroy(error => console.log('@session', error))
       return res.clearCookie('token').sendStatus(200)
     } catch (error) {
       console.log('@sign/out', error)
