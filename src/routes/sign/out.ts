fc671e0534de5e56c89347c352248ee28ea2b7db
NorahSketch <norahmariano@gmail.com>
2021-12-06 10:39:29 +0100

    Null Cookie

--- src/routes/sign/out.ts
+++ src/routes/sign/out.ts
@@ -1,5 +1,6 @@
 import type Route from '../../types/Route'
 import authorization from '../../middlewares/http'
+import env from '../../env/env'
 
 // TODO: Fix Sign Out
 
@@ -8,7 +9,13 @@ const route: Route = {
   authorization,
   execute: async (req, res) => {
     try {
-      return res.clearCookie('token').sendStatus(200)
+      // return res.clearCookie('token').sendStatus(200)
+      return res.status(200).cookie('token', null, {
+        expires: new Date(Date.now() + 500),
+        secure: env.ENVIRONMENT === 'LIVE',
+        sameSite: env.ENVIRONMENT === 'LIVE' ? 'none' : 'lax',
+        httpOnly: true
+      })
     } catch (error) {
       console.log('@sign/out', error)
       return res.status(500)
