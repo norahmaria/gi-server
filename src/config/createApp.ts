746f836b3f80bba52203099bf005c097b537934b
NorahSketch <norahmariano@gmail.com>
2021-12-06 16:39:11 +0100

    SameSite None

--- src/config/createApp.ts
+++ src/config/createApp.ts
@@ -18,7 +18,7 @@ const createApp = (cors: Cors) => {
     secret: env.SECRET,
     cookie: { 
       secure: env.ENVIRONMENT === 'LIVE',
-      sameSite: env.ENVIRONMENT === 'LIVE' ? 'lax' : 'lax',
+      sameSite: env.ENVIRONMENT === 'LIVE' ? 'none' : 'lax',
       httpOnly: true, 
       maxAge: 1000 * 60 * 60 * 24 
     }
