82d33ca0a0c4a6b70a70fefca6dc0ddc9e23628c
NorahSketch <norahmariano@gmail.com>
2021-12-08 15:27:22 +0100

    Cookie Check

--- src/middlewares/ws.ts
+++ src/middlewares/ws.ts
@@ -2,10 +2,13 @@ import { Socket } from 'socket.io'
 import jwt from 'jsonwebtoken'
 import env from '../env/env'
 
+// TODO: Fix Token not being added
+
 const authorization = (socket: Socket, next: (err?: any | undefined) => void) => {
   const { cookie } = socket.request.headers
   const token = cookie?.split('token=')[1]
 
+  console.log('@SOCKET HEADERS', socket.request.headers)
   console.log('@COOKIE VARIABLE', cookie)
   if (!token) return socket.disconnect()
 
